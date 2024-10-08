3c3934b605a2aa717d2fa7d453e993405dc04e62
Gjh99 <1737035429@qq.com>
2024-10-08 16:12:34 +0800

    feat(路由): 路由测试

--- src/layout/LayoutType/DefaultLayout/index.tsx
+++ src/layout/LayoutType/DefaultLayout/index.tsx
@@ -1,53 +1,61 @@
-import {Layout, TimePicker} from "antd";
+import {Layout, Menu} from "antd";
 import React from 'react';
 import './index.less'
 import Logo from "../components/Logo.tsx";
 import AvatarIcon from "../components/MyAvatar.tsx";
 import Setting from "../components/Setting.tsx";
+import {Outlet, useLocation, useNavigate} from "react-router-dom";
+import Language from "../components/Language.tsx";
 
-const {Header, Content} = Layout
+const {Header, Content, Sider} = Layout
 
 function Index() {
+    const navigate = useNavigate()
+    const location = useLocation()
+    console.log('useLocation', location)
+    const clickMenu = ({ key }) => {
+        console.log('key', key)
+        // const route = searchRoute(key, props.menuList);
+        // if (route.isLink) window.open(route.isLink, "_blank");
+        navigate(key);
+    };
     return (
         <>
             <Layout>
                 <Header className="flx-justify-between">
                     <Logo/>
                     <div className="mr10 flx-center h100">
+                        <Language/>
                         <Setting/>
                         <AvatarIcon/>
                     </div>
                 </Header>
 
                 <Layout>
-                    <TimePicker />
-                    {/*<Sider trigger={null} collapsible collapsed={collapsed}>*/}
-                    {/*    <Menu*/}
-                    {/*        theme="dark"*/}
-                    {/*        mode="inline"*/}
-                    {/*        defaultSelectedKeys={['1']}*/}
-                    {/*        items={[*/}
-                    {/*            {*/}
-                    {/*                key: '1',*/}
-                    {/*                icon: <UserOutlined/>,*/}
-                    {/*                label: 'nav 1',*/}
-                    {/*            },*/}
-                    {/*            {*/}
-                    {/*                key: '2',*/}
-                    {/*                icon: <VideoCameraOutlined/>,*/}
-                    {/*                label: 'nav 2',*/}
-                    {/*            },*/}
-                    {/*            {*/}
-                    {/*                key: '3',*/}
-                    {/*                icon: <UploadOutlined/>,*/}
-                    {/*                label: 'nav 3',*/}
-                    {/*            },*/}
-                    {/*        ]}*/}
-                    {/*    />*/}
-                    {/*</Sider>*/}
+                    <Sider trigger={null}>
+                        <Menu
+                            mode="inline"
+                            selectedKeys={[location.pathname]}
+                            onClick={clickMenu}
+                            items={[
+                                {
+                                    key: '/',
+                                    label: '首页',
+                                },
+                                {
+                                    key: '2',
+                                    label: 'nav 2',
+                                },
+                                {
+                                    key: '3',
+                                    label: 'nav 3',
+                                },
+                            ]}
+                        />
+                    </Sider>
                     <Layout>
                         <Content>
-                            {/*Content*/}
+                            <Outlet/>
                         </Content>
                     </Layout>
                 </Layout>
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,25 @@
-import {useRoutes} from "react-router";
+import {Navigate, useRoutes} from "react-router";
 import Layout from "../layout";
+import Home from '@/views/home'
 
 export const rootRouter = [
     {
-        path:'/',
-        element: <Layout/>
-    }
+        path: '/',
+        element: <Layout/>,
+        children: [
+            {
+                index: true, // 当路径为 `/` 时，重定向到 `/home`
+                element: <Navigate to="/home"/>,
+            },
+            {
+                path: 'home',
+                element: <Home/>,
+                title: '首页',
+                key: 'home'
+            }
+        ]
+    },
+
 ]
 
 const Router = () => {
