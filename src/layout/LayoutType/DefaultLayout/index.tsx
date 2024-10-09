8dd6aff4f9cbd1a7bb7ce28a495e16a5766e9d2a
Gjh99 <1737035429@qq.com>
2024-10-09 14:49:45 +0800

    fix(defaultLayout): 修改为公共菜单组件

--- src/layout/LayoutType/DefaultLayout/index.tsx
+++ src/layout/LayoutType/DefaultLayout/index.tsx
@@ -1,24 +1,16 @@
-import {Layout, Menu} from "antd";
+import {Layout} from "antd";
 import React from 'react';
 import './index.less'
 import Logo from "../components/Logo.tsx";
 import AvatarIcon from "../components/MyAvatar.tsx";
 import Setting from "../components/Setting.tsx";
-import {Outlet, useLocation, useNavigate} from "react-router-dom";
+import {Outlet} from "react-router-dom";
 import Language from "../components/Language.tsx";
+import MyMenu from "../components/Menu";
 
 const {Header, Content, Sider} = Layout
 
 function Index() {
-    const navigate = useNavigate()
-    const location = useLocation()
-    console.log('useLocation', location)
-    const clickMenu = ({ key }) => {
-        console.log('key', key)
-        // const route = searchRoute(key, props.menuList);
-        // if (route.isLink) window.open(route.isLink, "_blank");
-        navigate(key);
-    };
     return (
         <>
             <Layout>
@@ -33,25 +25,7 @@ function Index() {
 
                 <Layout>
                     <Sider trigger={null}>
-                        <Menu
-                            mode="inline"
-                            selectedKeys={[location.pathname]}
-                            onClick={clickMenu}
-                            items={[
-                                {
-                                    key: '/',
-                                    label: '首页',
-                                },
-                                {
-                                    key: '2',
-                                    label: 'nav 2',
-                                },
-                                {
-                                    key: '3',
-                                    label: 'nav 3',
-                                },
-                            ]}
-                        />
+                        <MyMenu mode={'inline'}/>
                     </Sider>
                     <Layout>
                         <Content>
