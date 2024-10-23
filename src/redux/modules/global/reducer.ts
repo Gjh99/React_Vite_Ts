983d1b29ae7f1bdac5180b36720dff4f4ba43962
Gjh99 <1737035429@qq.com>
2024-10-23 16:59:08 +0800

    feat(系统设置): 控制tabRoute显示隐藏

--- src/redux/modules/global/reducer.ts
+++ src/redux/modules/global/reducer.ts
@@ -1,11 +1,12 @@
 import {produce} from "immer";
-import {SET_LANGUAGE, SET_LAYOUT_TYPE, SET_THEME} from "../mutationType.ts";
+import {SET_LANGUAGE, SET_LAYOUT_TYPE, SET_THEME,SET_TAB} from "../mutationType.ts";
 import {GlobalState} from '@/redux/interface/index.ts'
 
 const globalState:GlobalState = {
     type: 'DefaultLayout',
     isDarkMode: false,
-    locale: 'zh'
+    locale: 'zh',
+    showTabs: true
 }
 
 const global = (state = globalState, action) =>
@@ -19,6 +20,10 @@ const global = (state = globalState, action) =>
                 break;
             case SET_LANGUAGE:
                 draftState.locale = action.globalLanguage
+                break;
+            case SET_TAB:
+                console.log(action.globalShowTabs)
+                draftState.showTabs = action.globalShowTabs
                 break;
             default:
                 break;
