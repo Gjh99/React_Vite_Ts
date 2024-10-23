e5fad7a909430d1d72405ab70f2a00e5579e8969
Gjh99 <1737035429@qq.com>
2024-10-23 15:46:14 +0800

    feat(公共类型): 加入tabs相关类型

--- src/interfaces/common.ts
+++ src/interfaces/common.ts
@@ -1,6 +1,16 @@
+import {setTabsRoute} from "../redux/modules/tabs/action.ts";
+
+interface TabsList {
+    title: string;
+    pathName: string;
+    key: string;
+}
+
 interface PropsInterFace {
     locale: string;
     setLanguage: (lang: string) => void;
     setLayout: (layout: string) => void;
     setTheme: (theme: string) => void;
+    TabsList: TabsList[]
+    setTabsRoute: (TabsList: TabsList[]) => void;
 }
