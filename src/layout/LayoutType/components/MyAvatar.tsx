39bc0abaaf16a68240a37fdce47c64292514c797
Gjh99 <1737035429@qq.com>
2024-10-29 16:18:57 +0800

    feat(用户图标): 加入下拉操作

--- src/layout/LayoutType/components/MyAvatar.tsx
+++ src/layout/LayoutType/components/MyAvatar.tsx
@@ -1,13 +1,36 @@
 import React from "react";
 import '../DefaultLayout/index.less'
-import {Avatar} from "antd";
+import {Avatar, Dropdown} from "antd";
 import reactImg from '@/assets/react.svg'
 import './index.less'
+
 function AvatarIcon() {
+    const userControlsItems = [
+        {
+            key: '1',
+            label: <div>个人信息</div>
+        },
+        {
+            key: '2',
+            label: <div>退出登录</div>
+        },
+    ]
+    const handleMenuClick = (e) => {
+        if (e.key == 2) {
+            console.log(e.key)
+        }
+    }
     return (
         <>
-            <div className="avatar flx-align-center">
-                <Avatar size="large" src={reactImg} />
+            <div className="avatar flx-align-center pointer">
+                <Dropdown
+                    menu={{
+                        items: userControlsItems,
+                        onClick: handleMenuClick
+                    }}
+                >
+                    <Avatar size="large" src={reactImg}/>
+                </Dropdown>
                 <div className="ml10 nickName">---</div>
             </div>
         </>
