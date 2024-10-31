779942f8bdd9957fdb521837befe569faed16b81
Gjh99 <1737035429@qq.com>
2024-10-31 09:46:43 +0800

    feat(mock): 加入mock

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import eslintPlugin from "vite-plugin-eslint";
-import { resolve } from 'path';
+import {resolve} from 'path';
+import {ViteMockOptions, viteMockServe} from "vite-plugin-mock";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +10,16 @@ export default defineConfig({
         react(),
         eslintPlugin({
             cache: false
-        })
+        }),
+        viteMockServe({
+            mockPath: 'src/mocks', // mock数据存放的文件夹路径
+            localEnabled: true, // 是否启用本地mock
+            prodEnabled: false, // 是否在生产环境中启用mock
+            injectCode: `
+        import { setupProdMockServer } from './mockProdServer';
+        setupProdMockServer();
+      `,
+        } as ViteMockOptions),
     ],
     resolve: {
         alias: {
