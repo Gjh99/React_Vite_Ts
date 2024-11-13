4c7e790995a90dc9baaa60bbc4b39393bf472daf
Gjh99 <1737035429@qq.com>
2024-11-13 16:01:19 +0800

    feat(redux): 设置菜单

--- src/redux/modules/auth/action.ts
+++ src/redux/modules/auth/action.ts
@@ -1,6 +1,11 @@
-import { SET_TOKEN } from "../mutationType.ts";
+import { SET_TOKEN, SET_MENU } from "../mutationType.ts";
 /* Token */
 export const setToken = (token) => ({
     type: SET_TOKEN,
     token
+})
+/* Token */
+export const setMenu = (menu) => ({
+    type: SET_MENU,
+    menu
 })
