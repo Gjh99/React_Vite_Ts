811fa6905bc6ffdbe72e3839d07c17ff8804d6f7
Gjh99 <1737035429@qq.com>
2024-11-13 15:59:58 +0800

    feat(路由): 路由

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,9 @@ import {Navigate, useRoutes} from "react-router";
 import Layout from "../layout";
 import Login from '@/views/login'
 import Home from '@/views/home'
+import User from '@/views/system/user'
+import Role from '@/views/system/role'
+import PictureConversion from '@/views/toolBox/PictureConversion'
 
 export const rootRouter = [
     {
@@ -24,13 +27,67 @@ export const rootRouter = [
                 path: '/home',
                 element: <Home/>,
                 meta:{
-                    title: '首页',
                     key: 'home',
                     requiresAuth: true
                 }
             }
+        ],
+        meta: {
+            title: "首页"
+        },
+    },
+    {
+        element: <Layout/>,
+        meta: {
+            title: "系统设置"
+        },
+        children:[
+            {
+                path: '/system/user',
+                element: <User/>,
+                meta:{
+                    title: '用户管理',
+                    key: 'user',
+                    requiresAuth: true
+                }
+            },
+            {
+                path: '/system/role',
+                element: <Role/>,
+                meta:{
+                    title: '角色管理',
+                    key: 'role',
+                    requiresAuth: true
+                }
+            },
         ]
-    }
+    },
+    {
+        element: <Layout/>,
+        meta: {
+            title: "工具箱"
+        },
+        children:[
+            {
+                path: '/Toolbox/PictureConversion',
+                element: <PictureConversion />,
+                meta:{
+                    title: '图片转换',
+                    key: 'PictureConversion',
+                    requiresAuth: true
+                }
+            },
+            {
+                path: '/system/role',
+                element: <Role/>,
+                meta:{
+                    title: '角色管理',
+                    key: 'role',
+                    requiresAuth: true
+                }
+            },
+        ]
+    },
 
 ]
 
