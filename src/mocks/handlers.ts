63cfe009a5b3b15caba7077711edd7d2f5dd0577
Gjh99 <1737035429@qq.com>
2024-11-13 15:57:56 +0800

    feat(mock): 菜单模拟接口

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -10,5 +10,14 @@ export default [
             const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
             return data;
         }
+    },
+    {
+        url: '/api/systemInterView',
+        method: 'get',
+        response: () => {
+            const dataPath = path.resolve(__dirname, './data/systemInterView.json');
+            const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+            return data;
+        }
     }
 ]
