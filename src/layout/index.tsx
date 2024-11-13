5e389fdcf08a6cfb45794492705c2b6ab5333977
Gjh99 <1737035429@qq.com>
2024-11-13 16:06:56 +0800

    fix(布局组件): 页面及ts相关问题

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,21 +1,21 @@
 import {setLayout} from "../redux/modules/global/action";
 import './index.less'
-import React, {Suspense, useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {connect} from "react-redux";
 
 const DefaultLayout = React.lazy(() => import('./LayoutType/DefaultLayout'))
 const SlideLayout = React.lazy(() => import('./LayoutType/SlideLayout'))
 const TopLayout = React.lazy(() => import('./LayoutType/TopLayout'))
 
-function MyLayout(props:any) {
-    let { type } = props
-    const [PageComponent, setPageComponent] = useState(null);
-    useEffect(()=>{
+function MyLayout(props: any) {
+    let {type, showTabs, showBreadcrumb} = props
+    const [PageComponent, setPageComponent] = useState<React.ComponentType<any> | null>(null);
+    useEffect(() => {
         setPageComponent(DefaultLayout)
-    },[])
+    }, [])
 
     useEffect(() => {
-        console.log('layoutState',type)
+        console.log('layoutState', type)
         switch (type) {
             case 'DefaultLayout':
                 setPageComponent(DefaultLayout);
@@ -31,14 +31,12 @@ function MyLayout(props:any) {
 
     return (
         <>
-           <Suspense fallback={<div>Loading...</div>}>
-                {PageComponent && <PageComponent/>}
-            </Suspense>
+            {PageComponent && <PageComponent showTabs={showTabs} showBreadcrumb={showBreadcrumb}/>}
         </>
     )
 }
 
-const mapStateToProps = (state) => state.global
+const mapStateToProps = (state:any) => state.global
 
 const mapDispatchToProps = {setLayout}
 
