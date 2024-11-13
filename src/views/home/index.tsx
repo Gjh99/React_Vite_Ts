14e1f74d88206ce23101ce432bc67dc8ccca71b6
Gjh99 <1737035429@qq.com>
2024-11-13 15:51:55 +0800

    feat(首页): 初始化内容

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,33 +1,38 @@
 import {Avatar, Card, Col, Row} from "antd";
-import React, {useRef, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import reactImg from '@/assets/react.svg';
 import EChart from "../../component/Echart.tsx";
 import './index.less';
-
-const dataList = [
-    {
-        label: '用户人数',
-        icon: 'icon-tuandui',
-        text: '上月新增用户数',
-        color: '#4c4cf5'
-    },
-    {
-        label: '访问量',
-        icon: 'icon-yonghufangwenliang',
-        text: '上月累计访问量',
-        color: '#f12020'
-    },
-    {
-        label: '在线人数',
-        icon: 'icon-zaixianrenshu',
-        text: '上月累计在线人数',
-        color: '#4c4cf5'
-    },
-]
+import {getSystemInterView} from "../../api/user.ts";
 
 function home() {
     const homeRef = useRef(null);
-    const [option] = useState({
+
+    const [systemData, setSystemData] = useState([
+        {
+            label: '用户人数',
+            icon: 'icon-tuandui',
+            text: '上月新增用户数',
+            color: '#4c4cf5',
+            num: null
+        },
+        {
+            label: '访问量',
+            icon: 'icon-yonghufangwenliang',
+            text: '上月累计访问量',
+            color: '#f12020',
+            num: null
+        },
+        {
+            label: '在线人数',
+            icon: 'icon-zaixianrenshu',
+            text: '上月累计在线人数',
+            color: '#4c4cf5',
+            num: null
+        },
+    ])
+
+    const [userOption, setUserOption] = useState({
         xAxis: {
             data: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月']
         },
@@ -35,10 +40,10 @@ function home() {
         series: [{
             name: '用户数',
             type: 'bar',
-            data: [5, 20, 36, 10, 10, 20, 5, 20, 5, 20, 5, 20]
+            data: null
         }]
     });
-    const [lineOption] = useState({
+    const [lineOption,setLineOption] = useState({
         tooltip: {
             trigger: 'axis'
         },
@@ -53,10 +58,39 @@ function home() {
         series: [{
             name: '访问量',
             type: 'line',
-            data: [120, 132, 101, 134, 90, 230, 210, 230, 210, 230, 210, 101],
+            data: null,
             areaStyle: {}
         }]
     });
+
+    const getSyetemData = async () => {
+        let res = await getSystemInterView();
+        let {code, data} = res;
+        if (code == 200) {
+            const systemDataClone_ = [...systemData]
+            let {addPeopleNum, interViewNum, onOnlineNum, userYearData, interViewYearData} = data
+            systemDataClone_[0].num = addPeopleNum;
+            systemDataClone_[1].num = interViewNum;
+            systemDataClone_[2].num = onOnlineNum;
+            setSystemData(systemDataClone_)
+            setUserOption(prevUserOption => ({
+                ...prevUserOption,
+                series: [{...prevUserOption.series[0], data: userYearData}]
+            }))
+            setUserOption(prevUserOption => ({
+                ...prevUserOption,
+                series: [{...prevUserOption.series[0], data: userYearData}]
+            }))
+            setLineOption(prevUserOption => ({
+                ...prevUserOption,
+                series: [{...prevUserOption.series[0], data: interViewYearData}]
+            }))
+        }
+    }
+
+    useEffect(() => {
+        getSyetemData()
+    }, []);
     return (
         <div ref={homeRef} className="home">
             <Card>
@@ -71,13 +105,18 @@ function home() {
 
             <Row gutter={16} className="mt10">
                 {
-                    dataList.map((item, index) => (
+                    systemData.map((item, index) => (
                         <Col span={8} key={index}>
                             <Card title={item.label} bordered={false}
                                   extra={<i className={`iconfont ${item.icon} fontSize18`}
                                             style={{color: item.color}}/>}>
-                                <div className="fontSize16">
-                                    {item.text}
+                                <div className="flx-justify-between">
+                                    <div className="fontSize16">
+                                        {item.text}
+                                    </div>
+                                    <div className="fontSize22 fontW" style={{color: item.color}}>
+                                        {item.num}
+                                    </div>
                                 </div>
                             </Card>
                         </Col>
@@ -87,7 +126,7 @@ function home() {
 
             <Card className="mt10">
                 <div className="chart">
-                    <EChart option={option}/>
+                    <EChart option={userOption}/>
                 </div>
             </Card>
 
