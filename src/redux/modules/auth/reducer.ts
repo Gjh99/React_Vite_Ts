06d19512adadb48727755c66dc164d88a68c52ee
Gjh99 <1737035429@qq.com>
2024-11-13 16:01:57 +0800

    feat(redux): 设置菜单

--- src/redux/modules/auth/reducer.ts
+++ src/redux/modules/auth/reducer.ts
@@ -1,9 +1,10 @@
 import {produce} from "immer";
-import {SET_TOKEN} from "../mutationType.ts";
+import {SET_TOKEN,SET_MENU} from "../mutationType.ts";
 import {AuthState  as AuthStateType} from '@/redux/interface/index.ts'
 
 const authState:AuthStateType = {
-    token: ''
+    token: '1',
+    menuList: []
 }
 
 const auth = (state = authState, action) => {
@@ -11,6 +12,10 @@ const auth = (state = authState, action) => {
         switch (action.type) {
             case SET_TOKEN:
                 draftState.token = action.token
+                break;
+            case SET_MENU:
+                console.log('menuList', action)
+                draftState.menuList = action.menu
                 break;
             default:
                 break;
