440241fdb0c8b77b32717e47d136a7f75c2d8938
Gjh99 <1737035429@qq.com>
2024-11-13 15:57:15 +0800

    feat(菜单): 菜单模拟数据

--- src/layout/LayoutType/components/Menu/index.tsx
+++ src/layout/LayoutType/components/Menu/index.tsx
@@ -1,40 +1,48 @@
 import {Menu} from "antd";
 import {useLocation, useNavigate} from "react-router-dom";
+import {getMenu} from "../../../../api/user.ts";
+import {useEffect} from "react";
+import { connect } from "react-redux";
+import {setMenu} from "../../../../redux/modules/auth/action.ts";
 
 const MyMenu = (props) => {
     const navigate = useNavigate()
     const location = useLocation()
-    let {mode} = {...props}
-    console.log(mode)
-    const clickMenu = ({ key }) => {
+    let {mode, setMenu, menuList} = {...props}
+    const clickMenu = ({key}) => {
         console.log('key', key)
         // const route = searchRoute(key, props.menuList);
         // if (route.isLink) window.open(route.isLink, "_blank");
         navigate(key);
     };
+
+    const getMenuData = async () => {
+        let res = await getMenu();
+        let {code} = res;
+        if (code == 200) {
+            setMenu(res.data)
+            console.log('res', props)
+        }
+    }
+
+    useEffect(() => {
+        getMenuData()
+    }, [])
+
     return (
         <>
             <Menu
                 mode={mode}
                 selectedKeys={[location.pathname]}
                 onClick={clickMenu}
-                items={[
-                    {
-                        key: '/home',
-                        label: '首页',
-                    },
-                    {
-                        key: '2',
-                        label: 'nav 2',
-                    },
-                    {
-                        key: '3',
-                        label: 'nav 3',
-                    },
-                ]}
+                items={menuList}
             />
         </>
     )
 }
 
-export default MyMenu
+const mapStateToProps = (state) => state.auth;
+
+const mapDispatchToProps = {setMenu}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MyMenu)
