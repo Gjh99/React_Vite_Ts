fdab205ca881c1635c08df4d3222f3bd7e4b5389
Gjh99 <1737035429@qq.com>
2024-12-12 16:22:10 +0800

    feat(redis):redis

--- main.ts
+++ main.ts
@@ -14,6 +14,14 @@ import { RoleService } from './src/role/services';
 import passport from "passport";
 import { Dict } from './src/dict/controller';
 import { DictService } from './src/dict/services';
+import { Captcha } from './src/captcha/controller';
+import { CaptchaService } from './src/captcha/services';
+import session from 'express-session';
+import Redis from 'ioredis';
+import {RedisStore} from 'connect-redis';
+
+ // 创建 Redis 客户端
+const redisClient = new Redis()
 
 const container = new Container();
 /**
@@ -22,6 +30,13 @@ const container = new Container();
  */
 
 container.bind(JWT).to(JWT)
+
+/**
+ * 验证码模块
+ */
+container.bind(Captcha).to(Captcha)
+container.bind(CaptchaService).to(CaptchaService)
+
 
 /* 
  user模块
@@ -51,8 +66,8 @@ container.bind(ToolBoxService).to(ToolBoxService)
 /**
  * prisma依赖注入
  */
-container.bind<PrismaClient>('PrismaClient').toFactory(()=>{
-    return () =>{
+container.bind<PrismaClient>('PrismaClient').toFactory(() => {
+    return () => {
         return new PrismaClient()
     }
 })
@@ -64,9 +79,19 @@ jwtService.JwtStrategy();  // 调用JwtStrategy注册JWT策略
 
 
 const server = new InversifyExpressServer(container);
-server.setConfig((app) =>{
+server.setConfig((app) => {
     //解析json数据
     app.use(express.json())
+
+    // 初始化session 
+    app.use(
+        session({
+        secret: 'node_service_123456', // 设置用于签名会话ID的密钥 
+        resave: false, // 是否强制保存会话即使未修改 
+        saveUninitialized: true, // 是否将未初始化的会话存储 
+        cookie: { maxAge: 60000 }, // 会话有效期 
+        store: new RedisStore({ client: redisClient }), 
+    }));
 
     // 初始化passport
     app.use(passport.initialize());
@@ -74,6 +99,6 @@ server.setConfig((app) =>{
 
 const app = server.build();
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log("服务端口3000启动");
 })
