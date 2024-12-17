fe84820992be0fbaf84ffeb737d845b3b2525c5b
Gjh99 <1737035429@qq.com>
2024-12-17 14:51:36 +0800

    fix(redux): 持久化

--- src/redux/persistConfig.ts
+++ src/redux/persistConfig.ts
@@ -3,7 +3,7 @@ import storage from "redux-persist/lib/storage";
 const persistConfig = {
     key: 'root', // 可选，用于在localStorage中设置键名
     storage, // 存储引擎，默认使用localStorage
-    blacklist: ['tabs']
+    // blacklist: ['tabs']
     // 其他配置选项...
 };
 
