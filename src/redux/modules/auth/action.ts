fe5ed6c6508c7db3dee22f841143644a817e97e9
Gjh99 <1737035429@qq.com>
2024-12-20 11:02:46 +0800

    fix(auth): 设置用户信息

--- src/redux/modules/auth/action.ts
+++ src/redux/modules/auth/action.ts
@@ -1,11 +1,11 @@
-import { SET_MENU } from "../mutationType";
-import {MenuList} from "@/redux/interface";
-/* Token */
-// export const setToken = (token:string) => ({
-//     type: SET_TOKEN,
-//     token
-// })
-/* Token */
+import {SET_MENU, SET_USERINFO} from "../mutationType";
+import {MenuList, userInfoInterface} from "@/redux/interface";
+/* UserInfo */
+export const setUserInfo = (userInfo:userInfoInterface) => ({
+    type: SET_USERINFO,
+    userInfo
+})
+/* Menu */
 export const setMenu = (menuList:MenuList[]) => ({
     type: SET_MENU,
     menuList
