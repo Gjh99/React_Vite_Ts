cf75192ff33c2aa63ea36149245dab840c4929ee
Gjh99 <1737035429@qq.com>
2024-12-24 14:33:24 +0800

    fix(logo): logo

--- src/layout/LayoutType/components/Logo.tsx
+++ src/layout/LayoutType/components/Logo.tsx
@@ -1,11 +1,13 @@
-import '../DefaultLayout/index.less'
 import logoImg from "@/assets/image/logo.png"
-function Logo() {
+import logoCollapsed from "@/assets/image/logoCollapsed.png"
+
+const Logo = (prop: any) => {
     return (
         <>
-            <div className="logo">
-                <div className="content">
-                    <img src={logoImg}/>
+            <div className={prop.collapsed ? 'logoCollapsed' : "logo"}>
+                <div className={prop.collapsed ? '' : "content"}>
+                    <img className={prop.collapsed ? 'logoCollapsedImg' : ''}
+                         src={prop.collapsed ? logoCollapsed : logoImg}/>
                 </div>
             </div>
         </>
